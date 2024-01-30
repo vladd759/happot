f3d3641822c1232d56d16a8b144a86c01072d5bc
vladd759 <158172356+vladd759@users.noreply.github.com>
2024-01-30 15:18:16 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.InBackground)
+basic.forever(function () {
+	
+})
+control.inBackground(function () {
+    serial.writeNumber(5)
+})
+led.toggle(5,10)
+9*5133
+game.setScore(0-10000000)
+game.addLife(10)
+game.gameOver()
+game.isRunning
+game.isPaused()
+game.removeLife(1)
+game.setLife(1000)
+doSomething()
+function doSomething() {
+    
+}
+list.push(100)
+images.createImage(`
+. . . . .
+. . . . .
+. . # . .
+. . . . .
+. . . . .
+`)  = [1, 2, 3].createBigImage(
